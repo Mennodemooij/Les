9719f9a2718cb774354603b98b828ce1d8401ef5
Menno de Mooij <mennodemooij@hotmail.com>
2015-09-18 12:05:01 +0200

    code die te begrijpen is

---
+++ foreach.php
@@ -0,0 +1,11 @@
+<?php
+$aProduct = array('Boeken', 'Tas', 'Brood', 'Sokken',);
+
+	echo '<b>Producten:</b><br />';
+	foreach ($aProduct as $sProduct)
+	{
+		echo $sProduct.'<br />';
+	}
+
+	// stukje code die voor de ander te begrijpen is
+?>
